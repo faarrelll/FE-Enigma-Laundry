48eecfa6c2fc2699910a50ca0676e112faae56e5
Farrel Akbar <farrelakbar432@gmail.com>
2025-01-08 15:39:40 +0700

    feat: add attribute data-testid for cypress testing

--- src/components/ui/LoginForm.tsx
+++ src/components/ui/LoginForm.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import AuthApi from "../../api/AuthApi";
 import { useNavigate } from "react-router";
-import authenticationImage from "../../assets/authentication/index"
+import authenticationImage from "../../assets/authentication/index";
 import { ToastUtils } from "../../utils/ToastUtils";
 
 interface LoginProps {
-  setActivePage: (page: string) => void; 
+  setActivePage: (page: string) => void;
 }
 
 export const LoginForm: React.FC<LoginProps> = ({ setActivePage }) => {
@@ -24,40 +24,56 @@ export const LoginForm: React.FC<LoginProps> = ({ setActivePage }) => {
       console.error(error);
       ToastUtils({ message: "Login Failed", type: "error" })();
     }
-  }
+  };
 
   return (
     <div className="bg-[#f5fff6] w-full h-full rounded-r-2xl flex flex-col p-5 justify-center items-center  ">
       <div className="flex justify-end items-center"></div>
-      <h2 className="font-bold text-xl mt-5 mb-3" data-test-id="">Welcome Back!</h2>
+      <h2 className="font-bold text-xl mt-5 mb-3" data-testid="login-title">
+        Login
+      </h2>
       <p className="text-slate-500 mb-3">Enter your username and password</p>
       <div>
         <form onSubmit={loginHandle} className="flex flex-col gap-4">
           <div className="flex flex-row gap-1 items-center bg-white pl-4 rounded-lg">
             <div className="w-4 h-4  flex justify-center items-center">
-              <img src={authenticationImage.email} alt="" className="h-full w-full object-fill" />
+              <img
+                src={authenticationImage.username}
+                alt=""
+                className="h-full w-full object-fill"
+              />
             </div>
             <input
               type="text"
               name="username"
               id="username"
               placeholder="username"
+              data-testid="username-input"
               className="w-full h-10 rounded-lg p-3 outline-none"
             />
           </div>
           <div className="flex flex-row gap-1 items-center bg-white pl-4 rounded-lg">
             <div className="w-4 h-4  flex justify-center items-center">
-              <img src={authenticationImage.padlock} alt="" className="h-full w-full object-fill" />
+              <img
+                src={authenticationImage.padlock}
+                alt=""
+                className="h-full w-full object-fill"
+              />
             </div>
             <input
               type="password"
               name="password"
               id="password"
+              data-test-id="password-input"
               placeholder="password"
               className="w-full h-10 rounded-lg p-3 outline-none"
             />
           </div>
-          <button className="bg-[#8dd07b] w-full h-10 mt-3 rounded-full text-white font-bold" type="submit">
+          <button
+            className="bg-[#8dd07b] w-full h-10 mt-3 rounded-full text-white font-bold"
+            type="submit"
+            data-testid="login-button"
+          >
             Login
           </button>
           <p className="text-sm text-center mt-2 mb-5">
