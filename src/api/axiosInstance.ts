9fa1af94c588ce7fb6799881034251c7880178fc
laksanii <prazetyo111@gmail.com>
2025-01-08 14:53:26 +0700

    fix: fix token axiosIntance

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -8,9 +8,7 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  // const token = localStorage.getItem("token");
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJlbmlnbWFjYW1wIiwiZXhwIjoxNzM2MzI0MzY0LCJpYXQiOjE3MzYzMjA3NjQsInVzZXJJZCI6ImRhNGFkODhiLTk5YjItNGJkZi04Y2M3LTU2M2Q0NjFkNTBlZSIsInJvbGUiOiJhZG1pbiIsInNlcnZpY2VzIjpudWxsfQ.ABQaV_fC3vouri7ZCpstWIMfWMPlZmoiohkkvVqEH8I";
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
